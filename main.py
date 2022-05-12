97d9dd760ff19e56397cbc0e268cd47b5d193ebc
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-05-13 00:55:48 +0700

    commit changes

--- main.py
+++ main.py
@@ -380,7 +380,6 @@ finally:
         print('Connection with SQL is closed')
 
 
-print("hello kasym!")
 # def new_window():
 #     tk.Label(win1, image=rnb_image).grid(row=0, column=0, stick='w')
 #     win1.update()
