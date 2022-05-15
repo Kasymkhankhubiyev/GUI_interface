c436fb8355c5196fc3178b0e7b94154f9f5478e1
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-05-15 19:04:16 +0700

    I made a function that can change existing recipe - change an amount of a component or delete a component.
    plan:
    done: to add an add_new_product function
    - to add an add_recipe function
    done: to add a change_recipe function - products from prepared list
    done: to add a delete_product function with caution

--- admin.py
+++ admin.py
@@ -24,8 +24,9 @@ class Admin:
         self.outprint.place(x=250, y=300, width=300)
         tk.Button(self.admin_table, command=self.enter_program, text='Enter', font=('Arial', 14)).place(x=350, y=250)
 
-        self.button_list=[]
-        self.label_list=[]
+        self.button_list = []
+        self.label_list = []
+        self.row_counter = 0
 
     def enter_program(self):
         value1 = self.login.get()
@@ -60,41 +61,144 @@ class Admin:
         self.change_price_module()
         self.add_item_module()
         self.delete_item_module()
+        self.add_recipe_module()
+        self.update_recipe_module()
 
     def change_price_module(self):
-        tk.Label(self.admin_table, text='Изменить цену товара', font=('Arial', 12)).grid(row=0, column=1, pady=5)
+        tk.Label(self.admin_table, text='Изменить цену товара', font=('Arial', 12)).grid(row=self.row_counter, column=1, pady=5)
         item_list = self.get_items_name()
         self.change_price_combobox = ttk.Combobox(self.admin_table, values=item_list, font=('Arial', 12), state='readonly')
-        self.change_price_combobox.grid(row=1, column=0, pady=5)
+        self.change_price_combobox.grid(row=self.row_counter+1, column=0, pady=5)
         self.change_price_combobox.bind("<<ComboboxSelected>>", self.get_item_price)
         self.change_price_txt = tk.StringVar(value='')
-        self.change_price_Lable = tk.Label(self.admin_table, width=10, textvariable=self.change_price_txt, font=('Arial', 12), relief='sunken').grid(row=1, column=1, pady=5)
+        self.change_price_Lable = tk.Label(self.admin_table, width=10, textvariable=self.change_price_txt, font=('Arial', 12), relief='sunken').grid(row=self.row_counter+1, column=1, pady=5)
         self.change_price_entry = tk.Entry(self.admin_table, font=('Arial', 12), width=10)
-        self.change_price_entry.grid(row=1, column=2, pady=5)
-        tk.Button(self.admin_table, command=self.update_product_cost, text='сохранить', font=('Arial', 12)).grid(row=1, column=3, pady=5)
+        self.change_price_entry.grid(row=self.row_counter+1, column=2, pady=5)
+        tk.Button(self.admin_table, command=self.update_product_cost, text='сохранить', font=('Arial', 12)).grid(row=self.row_counter+1, column=3, pady=5)
+        self.row_counter += 2
 
     def add_item_module(self):
-        tk.Label(self.admin_table, text='Добавить товар', font=('Arial', 12)).grid(row=2, column=1, pady=10)
-        tk.Label(self.admin_table, text='Наименование', font=('Arial', 12)).grid(row=3, column=0, pady=5)
+        tk.Label(self.admin_table, text='Добавить товар', font=('Arial', 12)).grid(row=self.row_counter, column=1, pady=10)
+        tk.Label(self.admin_table, text='Наименование', font=('Arial', 12)).grid(row=self.row_counter+1, column=0, pady=5)
         self.add_item_name = tk.Entry(self.admin_table, font=('Arial', 12), width=25)
-        self.add_item_name.grid(row=4, column=0, pady=5, padx=5, sticky='w')
-        tk.Label(self.admin_table, text='Объем\масса', font=('Arial', 12)).grid(row=3, column=1, padx=10, pady=5)
+        self.add_item_name.grid(row=self.row_counter+2, column=0, pady=5, padx=5, sticky='w')
+        tk.Label(self.admin_table, text='Объем\масса', font=('Arial', 12)).grid(row=self.row_counter+1, column=1, padx=10, pady=5)
         self.add_item_amount = tk.Entry(self.admin_table, width=10, font=('Arial',12))
-        self.add_item_amount.grid(row=4, column=1, pady=5)
-        tk.Label(self.admin_table, text='Тип товара', font=('Arial', 12)).grid(row=3, column=2, pady=5)
+        self.add_item_amount.grid(row=self.row_counter+2, column=1, pady=5)
+        tk.Label(self.admin_table, text='Тип товара', font=('Arial', 12)).grid(row=self.row_counter+1, column=2, pady=5)
         self.add_item_type = ttk.Combobox(self.admin_table, values=['кофе', 'чай', 'авторский', 'коктейл', 'выпечка'], font=('Arial', 12), width=10, state='readonly')
-        self.add_item_type.grid(row=4, column=2, pady=5)
-        tk.Label(self.admin_table, text='Цена', font=('Arial', 12)).grid(row=3, column=3, pady=5)
+        self.add_item_type.grid(row=self.row_counter+2, column=2, pady=5)
+        tk.Label(self.admin_table, text='Цена', font=('Arial', 12)).grid(row=self.row_counter+1, column=3, pady=5)
         self.add_item_cost = tk.Entry(self.admin_table, width=10, font=('Arial', 12))
-        self.add_item_cost.grid(row=4, column=3, pady=5, padx=10)
-        tk.Button(self.admin_table, command=self.add_new_product, text='сохранить', font=('Arial', 12)).grid(row=4, column=4, pady=5, padx=10)
+        self.add_item_cost.grid(row=self.row_counter+2, column=3, pady=5, padx=10)
+        tk.Button(self.admin_table, command=self.add_new_product, text='сохранить', font=('Arial', 12)).grid(row=self.row_counter+2, column=4, pady=5, padx=10)
+        self.row_counter+=3
 
     def delete_item_module(self):
-        tk.Label(self.admin_table, text='Удалить товар', font=('Arial', 12)).grid(row=5, column=1, padx=10, pady=5)
+        tk.Label(self.admin_table, text='Удалить товар', font=('Arial', 12)).grid(row=self.row_counter, column=1, padx=10, pady=5)
         values = self.get_items_name()
         self.delete_item_name = ttk.Combobox(self.admin_table, values=values, width=30, state='readonly')
-        self.delete_item_name.grid(row=6, column=0, padx=5, pady=5)
-        tk.Button(self.admin_table, command=self.delete_product, text='удалить', font=('Arial, 12')).grid(row=6, column=2, pady=5, padx=10)
+        self.delete_item_name.grid(row=self.row_counter+1, column=0, padx=5, pady=5)
+        tk.Button(self.admin_table, command=self.delete_product, text='удалить', font=('Arial, 12')).grid(row=self.row_counter+1, column=2, pady=5, padx=10)
+        self.row_counter += 2
+
+    def add_recipe_module(self):
+        tk.Label(self.admin_table, text='Добавить рецепт', font=('Arial', 12)).grid(row=self.row_counter, column=1, padx=10, pady=5)
+
+        self.row_counter+=1
+        pass
+
+    def update_recipe_module(self):
+        tk.Label(self.admin_table, text='Обновить рецептуру', font=('Arial', 12)).grid(row=self.row_counter, column=1, padx=10, pady=5)
+        tk.Label(self.admin_table, text='Наименование товара', font=('Arial', 12)).grid(row=self.row_counter+1, column=0, padx=10, pady=5)
+        self.update_recipe_item = ttk.Combobox(self.admin_table, values=self.get_items_name(), font=('Arial', 12), state='readonly')
+        self.update_recipe_item.grid(row=self.row_counter+2, column=0, padx=10, pady=5)
+        self.update_recipe_item.bind("<<ComboboxSelected>>", self.get_recipe_list)
+        self.update_recipe_row = self.row_counter+2
+        self.update_recipe_items = []
+        tk.Label(self.admin_table, text='Компонент', font=('Arial', 12)).grid(row=self.row_counter+1, column=1, padx=5, pady=5)
+        tk.Label(self.admin_table, text='Объем/Масса', font=('Arial', 12)).grid(row=self.row_counter+1, column=2, padx=5, pady=5)
+        tk.Label(self.admin_table, text='Действие', font=('Arial', 12)).grid(row=self.row_counter+1, column=3, padx=5, pady=5)
+        self.update_recipe_command = ttk.Combobox(self.admin_table, values=['Обновить', 'Удалить'], width=10, font=('Arial', 12), state='readonly')
+        self.update_recipe_command.grid(row=self.row_counter+2, column=3, pady=5, padx=5)
+        tk.Button(self.admin_table, text='Выполнить', font=('Arial', 12), command=self.update_recipe_execute).grid(row=self.row_counter+2, column=4, padx=5, pady=5)
+        self.row_counter += 3
+
+    pass
+
+    def get_recipe_list(self, event):
+        if self.update_recipe_item.get() != '':
+            sql = """SELECT id FROM items WHERE item_name = ?"""
+            cursor = self.data_base.cursor()
+            cursor.execute(sql, [self.update_recipe_item.get()])
+            self.data_base.commit()
+            item_name = cursor.fetchall()
+            self.item_id = item_name[0][0]
+
+            sql = 'SELECT food_name FROM recipe WHERE product_id = ?'
+            cursor.execute(sql, [item_name[0][0]])
+            self.data_base.commit()
+            array = cursor.fetchall()
+            cursor.close()
+            for arr in array:
+                self.update_recipe_items.append(arr[0])
+            self.update_recipe_item_box = ttk.Combobox(self.admin_table, values=self.update_recipe_items, font=('Arial', 12), state='readonly', width=15)
+            self.update_recipe_item_box.grid(row=self.update_recipe_row, column=1, padx=5, pady=5)
+            self.update_recipe_item_box.bind("<<ComboboxSelected>>", self.set_update_recipe_amount)
+            self.update_recipe_amount = tk.Entry(self.admin_table, width=10, font=('Arial', 12))
+            self.update_recipe_amount.grid(row=self.update_recipe_row, column=2, padx=5, pady=5)
+            self.window.update()
+        else:
+            pass
+
+    def set_update_recipe_amount(self, event):
+        if self.update_recipe_item_box.get() != '':
+            sql = """SELECT mass_gr FROM recipe WHERE food_name = ?"""
+            cursor = self.data_base.cursor()
+            cursor.execute(sql, [self.update_recipe_item_box.get()])
+            self.data_base.commit()
+            amount = cursor.fetchall()
+            cursor.close()
+            self.update_recipe_amount.delete(0, 'end')
+            self.update_recipe_amount.insert(0, amount[0])
+            self.window.update()
+        else:
+            pass
+
+    def update_recipe_execute(self):
+        if self.update_recipe_item.get() != '':
+            if self.update_recipe_item_box.get() != '':
+                if self.update_recipe_amount.get() != '':
+                    if self.update_recipe_amount.get().isdigit() or check_float(self.update_recipe_amount.get()):
+                        if self.update_recipe_command.get != '':
+                            if self.update_recipe_command.get() == 'Обновить':
+                                sql = """UPDATE recipe SET mass_gr = ? WHERE product_id = ? AND food_name = ?"""
+                                cursor = self.data_base.cursor()
+                                amount = convert_to_float(self.update_recipe_amount.get())
+                                cursor.execute(sql, [amount, self.item_id, self.update_recipe_item_box.get()])
+                                self.data_base.commit()
+                                cursor.close()
+                                self.window.update()
+                            else:
+                                if messagebox.askyesno(title='Удаление из рецепта', message='Вы уверены, что хотите удалить компонент из рецепта?'):
+                                    sql = """DELETE FROM recipe WHERE product_id = ? AND food_name = ?"""
+                                    cursor = self.data_base.cursor()
+                                    cursor.execute(sql, [self.item_id, self.update_recipe_item_box.get()])
+                                    self.data_base.commit()
+                                    cursor.close()
+                                    self.window.update()
+                                else:
+                                    pass
+                        else:
+                            messagebox.showerror(title='Упс... Ошибка', message='Выберите действие!')
+                    else:
+                        messagebox.showerror(title='Упс... Ошибка', message='Масса или объем должно быть числовым значением. Например: 170 или 170,5 или 170.5')
+                else:
+                    messagebox.showerror(title='Упс... Ошибка', message='Строка кол-ва пустая. Введите массу в гр или объем в мл. Например, 170.')
+            else:
+                messagebox.showerror(title='Упс... Ошибка', message='Выберите компонент.')
+        else:
+            messagebox.showerror(title='Упс... Ошибка', message='Выберите товарную позицию.')
 
     def get_item_price(self, event="<Button>"):
         sql = ("""SELECT cost FROM items where item_name = ?""")
@@ -155,7 +259,7 @@ class Admin:
             messagebox.showerror(title='Упс... Ошибка', message='Пустое поле названия товара!')
 
 
-    def add_recepe(self):
+    def add_recipe(self):
         pass
 
     def delete_product(self):
@@ -200,6 +304,24 @@ def check_str(str):
         return False
     else:
         return True
+
+
+def check_float(str):
+    if str.replace('.', '', 1).isdigit():
+        return True
+    elif str.replace(',', '', 1).isdigit():
+        return True
+    else:
+        return False
+
+
+def convert_to_float(str):
+    check = str
+    if check.replace('.', '', 1).isdigit():
+        return float(str)
+    elif check.replace(',', '', 1).isdigit():
+        return float(str.replace(',', '.', 1))
+
 
 def item_type_case(str):
     if str == 'кофе':
